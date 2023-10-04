8a44287581789db484ee91d3fc1f39d02fb89c4d
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-03 23:08:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io=require("socket.io")(7832,
     {
-        cors:{origin:"https://buyandsell-e7tw.onrender.com:7832"}
+        cors:{origin:"https://buyandsell-e7tw.onrender.com"}
     })
     let users=[]
     let targetUser=[]
